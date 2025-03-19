4ddaa1a457441002abc18957b73644036b7ea111
Beacon Street Studios <143728792+Beacon-Street-Studios@users.noreply.github.com>
2025-03-19 11:58:59 -0400

    changing audio file names

--- config.js
+++ config.js
@@ -5,31 +5,31 @@ config.duration = 4.0;
 config.defaultPlaybackRate = 2.0;
 
 let swellSamples = [
-    {file: 'VISION/SWELL/bss_honeywell_swell_vision_', count:21, tag: 'vision'},
-    {file: 'FUTURE/SWELL/bss_honeywell_swell_future_', count:24, tag: 'future'},
-    {file: 'OPTIMISM/SWELL/bss_honeywell_swell_optimism_', count:23, tag: 'optimism'},
-    {file: 'INNOVATION/SWELL/bss_honeywell_swell_innovation_', count:27, tag: 'innovation'},
+    {file: 'VISION/SWELL/bss_lucid_swell_vision_', count:21, tag: 'vision'},
+    {file: 'FUTURE/SWELL/bss_lucid_swell_future_', count:24, tag: 'future'},
+    {file: 'OPTIMISM/SWELL/bss_lucid_swell_optimism_', count:23, tag: 'optimism'},
+    {file: 'INNOVATION/SWELL/bss_lucid_swell_innovation_', count:27, tag: 'innovation'},
 ];
 
 let chimeSamples = [
-    {file: 'VISION/CHIME/bss_honeywell_chime_vision_', count:21, tag: 'vision'},
-    {file: 'FUTURE/CHIME/bss_honeywell_chime_future_', count:25, tag: 'future'},
-    {file: 'OPTIMISM/CHIME/bss_honeywell_chime_optimism_', count:23, tag: 'optimism'},
-    {file: 'INNOVATION/CHIME/bss_honeywell_chime_innovation_', count:26, tag: 'innovation'},
+    {file: 'VISION/CHIME/bss_lucid_chime_vision_', count:21, tag: 'vision'},
+    {file: 'FUTURE/CHIME/bss_lucid_chime_future_', count:25, tag: 'future'},
+    {file: 'OPTIMISM/CHIME/bss_lucid_chime_optimism_', count:23, tag: 'optimism'},
+    {file: 'INNOVATION/CHIME/bss_lucid_chime_innovation_', count:26, tag: 'innovation'},
 ];
 
 let melodySamples = [
-    {file: 'VISION/MELODY/bss_honeywell_melody_vision_', count:7, tag: 'vision'},
-    {file: 'FUTURE/MELODY/bss_honeywell_melody_future_', count:9, tag: 'future'},
-    {file: 'OPTIMISM/MELODY/bss_honeywell_melody_optimism_', count:13, tag: 'optimism'},
-    {file: 'INNOVATION/MELODY/bss_honeywell_melody_innovation_', count:11, tag: 'innovation'},
+    {file: 'VISION/MELODY/bss_lucid_melody_vision_', count:7, tag: 'vision'},
+    {file: 'FUTURE/MELODY/bss_lucid_melody_future_', count:9, tag: 'future'},
+    {file: 'OPTIMISM/MELODY/bss_lucid_melody_optimism_', count:13, tag: 'optimism'},
+    {file: 'INNOVATION/MELODY/bss_lucid_melody_innovation_', count:11, tag: 'innovation'},
 ];
 
 let percussionSamples = [
-    {file: 'VISION/PERCUSSION/bss_honeywell_percussion_vision_', count:9, tag: 'vision'},
-    {file: 'FUTURE/PERCUSSION/bss_honeywell_percussion_future_', count:14, tag: 'future'},
-    {file: 'OPTIMISM/PERCUSSION/bss_honeywell_percussion_optimism_', count:9, tag: 'optimism'},
-    {file: 'INNOVATION/PERCUSSION/bss_honeywell_percussion_innovation_', count:16, tag: 'innovation'},
+    {file: 'VISION/PERCUSSION/bss_lucid_percussion_vision_', count:9, tag: 'vision'},
+    {file: 'FUTURE/PERCUSSION/bss_lucid_percussion_future_', count:14, tag: 'future'},
+    {file: 'OPTIMISM/PERCUSSION/bss_lucid_percussion_optimism_', count:9, tag: 'optimism'},
+    {file: 'INNOVATION/PERCUSSION/bss_lucid_percussion_innovation_', count:16, tag: 'innovation'},
 ];
 
 let swellSprite = {
